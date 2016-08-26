7ef85bdbde7663be9c9c40150d4b6dcbac575df0
RedRoosterFarm <superchickenboy@gmail.com>
2016-08-26 19:12:45 -0400

    Update browser_css_chk.js

--- html/js/browser_css_chk.js
+++ html/js/browser_css_chk.js
@@ -1,4 +1,18 @@
-var m
+/*
+   Copyright 2016 Samuel H. Walker
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 $('title').ready(function() {
   var a = document.getElementById('css_link');
   if (navigator.userAgent.match(/Android/i) ||
@@ -12,10 +26,8 @@ $('title').ready(function() {
              ) {
     console.log("mobile")
     a.href = "/css/mobile.css";
-    m = 1;
   }
   else {
-    m = 0;
     console.log("not mobile");
     a.href = "/css/desktop.css";
   }
