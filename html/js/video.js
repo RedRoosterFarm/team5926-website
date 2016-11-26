77f7194e126d39c8de7d0ba99257fbbcbdb6461a
RedRoosterFarm <superchickenboy@gmail.com>
2016-11-25 22:08:38 -0500

    Wait to load YT video until page is loaded . Fixes the weird CSS Problems that would exist until video would load.

---
+++ html/js/video.js
@@ -0,0 +1,4 @@
+$(window).bind( "load", function () {
+  var vdiv = "videoDiv";
+  document.getElementById(vdiv).innerHTML = '<iframe width="480" height="270" src="https://www.youtube.com/embed/i1QyM9WTF18" allowfullscreen id="FIRSTvideo"></iframe>';
+});
