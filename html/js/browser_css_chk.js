f017c952e7940f4026bc44fe609cc24303769251
RedRoosterFarm <superchickenboy@gmail.com>
2017-06-13 19:24:37 -0400

    Revert "Merge remote-tracking branch 'origin/dev' into dev"
    This reverts commit 82caefa5b87ea50b0c21969489e09d728aaf66d2, reversing
    changes made to 555a01b20e06416870e2d592d73b668e78768190.

---
+++ html/js/browser_css_chk.js
@@ -0,0 +1,34 @@
+/*
+   Copyright 2016 Samuel H. Walker
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+$('title').ready(function() {
+  var a = document.getElementById('css_link');
+  if (navigator.userAgent.match(/Android/i) ||
+             navigator.userAgent.match(/webOS/i) ||
+             navigator.userAgent.match(/iPhone/i) ||
+             navigator.userAgent.match(/iPad/i) ||
+             navigator.userAgent.match(/iPod/i) ||
+             navigator.userAgent.match(/BlackBerry/) || 
+             navigator.userAgent.match(/Windows Phone/i) || 
+             navigator.userAgent.match(/ZuneWP7/i)
+             ) {
+    console.log("mobile")
+    a.href = "/css/mobile.css";
+  }
+  else {
+    console.log("not mobile");
+    a.href = "/css/desktop.css";
+  }
+});
